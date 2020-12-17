56c02f4b3da171051f180c6189b8517511d1123d
Gribouillage <2242125522@qq.com>
2020-12-18 01:37:19 +0800

    Solve 714

---
+++ Java/_0714/by_Greedy/Solution.java
@@ -0,0 +1,17 @@
+package _0714.by_Greedy;
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int tmp = prices[0];
+        int res = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(tmp > prices[i]) {
+                tmp = prices[i];
+            }else if(prices[i] > tmp + fee){
+                res += (prices[i] - fee - tmp);
+                tmp = prices[i] - fee;
+            }
+        }
+        return res;
+    }
+}
